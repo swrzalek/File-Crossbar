c080cc1d61a3c061656db4461dff393f99ddd1ae
Sebastian <stian.uek@gmail.com>
2019-03-26 23:03:41 +0100

    test verison

--- app.js
+++ app.js
@@ -13,6 +13,28 @@
  */
 const express = require('express');
 const aws = require('aws-sdk');
+var rn = require('random-number');
+var firebase = require('firebase');
+var admin = require("firebase-admin");
+var md5 = require('md5');
+// Random numbers generator config
+var gen = rn.generator({
+    min: 100000
+    , max: 999999
+    , integer: true
+})
+// Firebase config
+var config = {
+    apiKey: "AIzaSyC71XbnQxHPXYEeqdou8Q-eVgFlBq0uk1E",
+    authDomain: "crossbarfile.firebaseapp.com",
+    databaseURL: "https://crossbarfile.firebaseio.com",
+    projectId: "crossbarfile",
+    storageBucket: "crossbarfile.appspot.com",
+    messagingSenderId: "149186962608"
+};
+firebase.initializeApp(config);
+var database = firebase.database()
+
 
 /*
  * Set-up and run the Express app.
@@ -21,29 +43,19 @@ const app = express();
 app.set('views', './views');
 app.use(express.static('./public'));
 app.engine('html', require('ejs').renderFile);
-
-
-app.listen(process.env.PORT || 3000);
-
- 
-
-/* heroku config:set AWS_ACCESS_KEY_ID=AKIAJVAANWFHFYYWPFDA AWS_SECRET_ACCESS_KEY=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
- * Configure the AWS region of the target bucket.
- * Remember to change this to the relevant region.
- */ 
+app.listen(process.env.PORT || 3000 , console.log('App running !'));
 aws.config.region = 'eu-west-2';
-
 /*
  * Load the S3 information from the environment variables.
  */
 const S3_BUCKET = process.env.S3_BUCKET;
-
-
 /*
  * Respond to GET requests to /account.
  * Upon request, render the 'account.html' web page in views/ directory.
  */
+app.get('/', (req, res) => res.render('index.html'));
 app.get('/account', (req, res) => res.render('account.html'));
+app.get('/rec', (req, res) => res.render('receive.html'));
 
 /*
  * Respond to GET requests to /sign-s3.
@@ -82,5 +94,38 @@ app.get('/sign-s3', (req, res) => {
  * a way that suits your application.
  */
 app.post('/save-details', (req, res) => {
-    res.send("done");
+  // TODO: Read POSTed form data and do something useful
 });
+
+app.get('/getdata', (req, res) => {
+    var md5_code = md5(req.query.code.toString());
+    console.log("Kod md5 wpisany: " + md5_code)
+    function findLinkInDatabase(md5_code) {
+        firebase.database().ref('/crossbarfile/' + md5_code).once('value').then(function (snapshot) {
+            var link = snapshot.child('link_address').val();
+            console.log(link);
+        });
+       
+    }
+    var finalLink = findLinkInDatabase(md5_code);
+    res.json({ link: finalLink });
+    console.log(findLinkInDatabase(md5_code));
+
+});  
+
+app.post('/returnrandom', (req, res) => {
+    var rm = gen();
+
+    console.log(rm.toString());
+    res.json({ "random": rm });
+    var md5_code = md5(rm.toString());
+    console.log("kod md5 wygenerowany: " + md5_code)
+    var url = "https://s3.eu-west-2.amazonaws.com/crossfilebar/t.jpg";
+    writeUserData(md5_code,url )
+    function writeUserData(code , link) {
+        firebase.database().ref('crossbarfile/' + code).set({
+            link_address:link
+                });
+    }
+    
+})
