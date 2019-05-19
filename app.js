e8710f6d6462aa55e8a92367a08772613d7ae7b3
Kalmar <stian.uek@gmail.com>
2019-05-19 11:45:36 -0700

    latest:

--- app.js
+++ app.js
@@ -44,50 +44,18 @@ app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/css', express.static(__dirname + '/css'));
-/*
- * Load the S3 information from the environment variables.
- */
-const S3_BUCKET = process.env.S3_BUCKET;
-/*
- * Respond to GET requests to /account.
- * Upon request, render the 'account.html' web page in views/ directory.
- */
-app.get('/send', (req, res) => res.render('index.html'));
-app.get('/receive', (req, res) => res.render('receive.html'));
+
+
+var send = require('./routes/sendRoutes')
+app.use('/send', send)
+var receive = require('./routes/receiveRoutes')
+app.use('/receive', receive)
+
 app.get("/", (req, res) => {
     res.render("home.html")
 })
 
-/*
- * Respond to GET requests to /sign-s3.
- * Upon request, return JSON containing the temporarily-signed S3 request and
- * the anticipated URL of the image.
- */
-app.get('/sign-s3', (req, res) => {
-  const s3 = new aws.S3();
-  const fileName = req.query['file-name'];
-    const fileType = req.query['file-type'];
-  const s3Params = {
-    Bucket: S3_BUCKET,
-    Key: fileName,
-    Expires: 60,
-    ContentType: fileType,
-    ACL: 'public-read'
-  };
 
-  s3.getSignedUrl('putObject', s3Params, (err, data) => {
-    if(err){
-      console.log(err);
-      return res.end();
-    }
-    const returnData = {
-      signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
-    };
-    res.write(JSON.stringify(returnData));
-    res.end();
-  });
-});
 
 /*
  * Respond to POST requests to /submit_form.
---
+++ routes/sendRoutes.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const aws = require('aws-sdk');
+const S3_BUCKET = process.env.S3_BUCKET;
+
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+})
+router.get('/', (req, res) => res.render('send.html'));
+
+
+/*
+ * Respond to GET requests to /sign-s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and
+ * the anticipated URL of the image.
+ */
+router.get('/sign-s3', (req, res) => {
+    const s3 = new aws.S3();
+    const fileName = req.query['file-name'];
+    const fileType = req.query['file-type'];
+    const s3Params = {
+        Bucket: S3_BUCKET,
+        Key: fileName,
+        Expires: 60,
+        ContentType: fileType,
+        ACL: 'public-read'
+    };
+
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.end();
+        }
+        const returnData = {
+            signedRequest: data,
+            url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+        };
+        res.write(JSON.stringify(returnData));
+        res.end();
+    });
+});
+module.exports = router
